6cda833fa87e6188e2ffc9d797df5db51a75fe27
Juan Carlos Guerrero Fernandez <juancarlos.gf02@gmail.com>
2016-03-26 01:37:15 -0500

    aplicando angular

--- Temporales/Principal/conexion/user_control.php
+++ Temporales/Principal/conexion/user_control.php
@@ -1,23 +1,17 @@
 <?php
-
 include_once 'connection.php';
-	
 	class User {
-		
 		private $db;
 		private $connection;
-		
-		function __construct() {
+			function __construct() {
 			$this -> db = new DB_Connection();
 			$this -> connection = $this->db->getConnection();
 		}
-		
-		public function does_user_exist($username,$password)
-		{
+			public function does_user_exist($username,$password){
 			$query = "Select * from employee where username='$username' and password = '$password' ";
 			$result = mysqli_query($this->connection, $query);
 			if(mysqli_num_rows($result)>0){
-				$json[''] = ' Bienvenido '.$username;
+				$json='BIENVENIDO '.$username;
 				echo json_encode($json);
 				mysqli_close($this -> connection);
 			}else{
@@ -32,34 +26,19 @@ include_once 'connection.php';
 				echo json_encode($json);
 				mysqli_close($this->connection);
 			}
-			
-		}
-		
+		}	
 	}
-	
-	
 	$user = new User();
 	if(isset($_POST['username'],$_POST['password'])) {
 		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
+		$password = $_POST['password'];	
 		if(!empty($username) && !empty($password)){
-			
 			$encrypted_password = md5($password);
 			$user-> does_user_exist($username,$password);
-			
 		}else{
 			echo json_encode("Debes completar ambos datos");
 		}
-		
 	}
-
-
-
-
-
-
-
 
 
 
