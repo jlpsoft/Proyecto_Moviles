e86da5066eae5d968bde120f5e0fdd43391f3ff3
1valero <josevalerovegazo@gmail.com>
2016-04-06 18:26:14 -0500

    V4
    web

---
+++ Temporales/WebApplication/pagina v4/v3/resources/js/main.js
@@ -0,0 +1,36 @@
+// Creación del módulo
+var angularRoutingApp = angular.module('angularRoutingApp', ['ngRoute']);
+
+// Configuración de las rutas
+angularRoutingApp.config(function($routeProvider) {
+
+	$routeProvider
+		.when('/addproduct', {
+			templateUrl	: 'Principal/addproduct.html',
+			controller 	: 'mainController'
+		})
+		.when('/viewproduct', {
+			templateUrl : 'Principal/viewproduct.html',
+			controller 	: 'aboutController'
+		})
+		.when('/modifyproduct', {
+			templateUrl : 'Principal/modifyproduct.html',
+			controller 	: 'contactController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+
+angularRoutingApp.controller('mainController', function($scope) {
+	$scope.message = 'Hola, Mundo!';
+});
+
+angularRoutingApp.controller('aboutController', function($scope) {
+	$scope.message = 'Esta es la página "Acerca de"';
+});
+
+angularRoutingApp.controller('contactController', function($scope) {
+	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
+});
