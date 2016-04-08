fe8d8e2355f0c01af4c7a2ba8d7521780337d872
yoelkill <elvis_abner@hotmail.com>
2016-04-08 00:44:31 -0500

    Revert "v5"
    This reverts commit 47a56d923fa85c2f46c765e53fa8632f9a185bc6.

--- Temporales/WebApplication/v5/conexion1.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('hostname', 'localhost');
-define('user', 'root');
-define('password', '');
-define('databaseName', 'bdinventarline');
-$connect = mysqli_connect(hostname, user, password, databaseName);
-
-?>
